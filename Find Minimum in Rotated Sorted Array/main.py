4ed1eca1f90908d9778477b3890b14c3cc3c9c0a
MOSES <akinmade220@gmail.com>
2025-10-12 22:30:17 +0100

    broke my jaw while coding tis

--- Find Minimum in Rotated Sorted Array/main.py
+++ Find Minimum in Rotated Sorted Array/main.py
@@ -2,11 +2,13 @@
 
 153. Find Minimum in Rotated Sorted Array
 
-Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example,
+the array nums = [0,1,2,4,5,6,7] might become:
 
 [4,5,6,7,0,1,2] if it was rotated 4 times.
 [0,1,2,4,5,6,7] if it was rotated 7 times.
-Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1],
+a[2], ..., a[n-2]].
 
 Given the sorted rotated array nums of unique elements, return the minimum element of this array.
 
@@ -32,16 +34,40 @@ inflection point, between 5 and 1
 
 """
 
-def findMin(nums:list):
-    if not nums:
-        return False
-    left, right = 0, len(nums) - 1
-
+def findMinimum(nums:list):
+    if not len(nums):
+        return 'Error'
+    left, right = 0, len(nums)-1
     while left < right:
         mid = (left + right) // 2
-        if nums[mid] > nums[right]:
+        if nums[mid] > nums[left]:
             left = mid + 1
         else:
-            right = mid  #since its possible that the mid can also be the smallest
+            right = mid
     return nums[left]
-print(findMin([3,4,5,1,2]))
+
+print(findMinimum([3,4,5,1,2]))
+
+
+
+
+
+
+
+
+
+
+
+# def findMin(nums:list):
+#     if not nums:
+#         return False
+#     left, right = 0, len(nums) - 1
+#
+#     while left < right:
+#         mid = (left + right) // 2
+#         if nums[mid] > nums[right]:
+#             left = mid + 1
+#         else:
+#             right = mid  #since its possible that the mid can also be the smallest
+#     return nums[left]
+# print(findMin([3,4,5,1,2]))
---
+++ Search in  rotated sorted array/main.py
@@ -0,0 +1,78 @@
+"""
+
+
+33. Search in Rotated Sorted Array
+
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+
+
+    Explanation
+
+
+The Core Logic:
+
+Check which half is sorted using nums[left] <= nums[mid]
+
+If left half is sorted:
+
+If target is within [nums[left], nums[mid]), search left
+
+Else search right
+
+If right half is sorted:
+
+If target is within (nums[mid], nums[right]], search right
+
+Else search left
+
+Why This Works:
+
+In a rotated array, one half is always properly sorted
+
+We can efficiently eliminate half the search space each iteration
+
+The distinct values guarantee no duplicates to complicate comparisons
+
+"""
+
+def search(nums:list, target:int):
+    if not nums:
+        return 'Error'
+    left, right = 0, len(nums) -1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+
+        """check if left half is sorted"""
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+    return -1
+
+print(search([4,5,6,7,0,1,2], 0))
